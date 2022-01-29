380464544a3157525a574baf41e4c77cf3b17d43
Lionidas19 <leon.katsaros@gmail.com>
2022-01-29 22:51:06 +0200

    stuff

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -5,13 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
     [SerializeField] private string Light = "Light";
     [SerializeField] private string Credits = "Credits";
 
     public void StartGame()
     {
         LightOrDark.light = true;
-        SceneManager.LoadScene(Light);
+        StartCoroutine(LoadLevel(Light));
+    }
+
+    IEnumerator LoadLevel(string light)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(light);
     }
 
     public void StartCredits()
